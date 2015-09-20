c85f8deabe43268a951ca32a48543754702b4039
Alvaro Bautista <alvarobp@gmail.com>
2015-09-20 19:16:53 +0200

    Chapter 8: Use dollar factory everywhere in tests

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -5,16 +5,16 @@ require_relative '../src/franc'
 describe 'Multi-Currency Money' do
   it 'supports multiplication' do
     five = Money.dollar(5)
-    expect(five.times(2)).to eq(Dollar.new(10))
-    expect(five.times(3)).to eq(Dollar.new(15))
+    expect(five.times(2)).to eq(Money.dollar(10))
+    expect(five.times(3)).to eq(Money.dollar(15))
   end
 
   it 'supports equality' do
-    expect(Dollar.new(5)).to eq(Dollar.new(5))
-    expect(Dollar.new(5)).not_to eq(Dollar.new(6))
+    expect(Money.dollar(5)).to eq(Money.dollar(5))
+    expect(Money.dollar(5)).not_to eq(Money.dollar(6))
     expect(Franc.new(5)).to eq(Franc.new(5))
     expect(Franc.new(5)).not_to eq(Franc.new(6))
-    expect(Franc.new(5)).not_to eq(Dollar.new(5))
+    expect(Franc.new(5)).not_to eq(Money.dollar(5))
   end
 
   it 'supports franc multiplication' do
