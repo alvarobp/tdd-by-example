733e3feab42a281a82f3084cd6615d016ad433b2
Alvaro Bautista <alvarobp@gmail.com>
2015-09-20 08:25:00 +0200

    Chapter 2: Test multiplication expecting dollar not to change
    We want to get rid of side effects

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -3,7 +3,9 @@ require_relative '../src/dollar'
 describe 'Multi-Currency Money' do
   it 'supports multiplication' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq(10)
+    product = five.times(2)
+    expect(product.amount).to eq(10)
+    product = five.times(3)
+    expect(product.amount).to eq(15)
   end
 end
