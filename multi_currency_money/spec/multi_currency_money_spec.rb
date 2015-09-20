3b9833b3cd7150ddf062153b92bf7779ba29dd4b
Alvaro Bautista <alvarobp@gmail.com>
2015-09-20 08:41:09 +0200

    Chapter 4: Rely on equality in first multiplication assertion

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -4,7 +4,7 @@ describe 'Multi-Currency Money' do
   it 'supports multiplication' do
     five = Dollar.new(5)
     product = five.times(2)
-    expect(product.amount).to eq(10)
+    expect(product).to eq(Dollar.new(10))
     product = five.times(3)
     expect(product.amount).to eq(15)
   end
