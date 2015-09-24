a26864253e77f2e52e56fd789b77849a96cd738e
Alvaro Bautista <alvarobp@gmail.com>
2015-09-24 21:28:54 +0200

    Chapter 14: Rates table in Bank
    Test errors
    
    Array in Ruby checks that elements are equal. Still used Pair to better
    follow up with the examples.
    
    There are no private classes in Ruby. They are usually represented
    by nesting them. It is not always obvious they are not used outside
    the scope they are declared in.
    
    Faking hash returning zero would not work, I implemented it as it is
    common in Ruby, calculating the hash of an Array containing all values.

--- multi_currency_money/src/bank.rb
+++ multi_currency_money/src/bank.rb
@@ -1,12 +1,36 @@
 class Bank
+  class Pair
+    def initialize(from, to)
+      @from = from
+      @to = to
+    end
+
+    def ==(other)
+      @from == other.from && @to == other.to
+    end
+    alias :eql? :==
+
+    def hash
+      [@from, @to].hash
+    end
+
+    protected
+    attr_reader :from, :to
+  end
+
+  def initialize
+    @rates = {}
+  end
+
   def reduce(source, to)
     source.reduce(self, to)
   end
 
   def add_rate(from, to, rate)
+    @rates[Pair.new(from, to)] = rate
   end
 
   def rate(from, to)
-    from == 'CHF' && to == 'USD' ? 2 : 1
+    @rates[Pair.new(from, to)]
   end
 end
