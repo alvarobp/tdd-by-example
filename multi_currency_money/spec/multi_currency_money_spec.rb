89895b40ab24fad823156173f1096a0cc1747aa0
Alvaro Bautista <alvarobp@gmail.com>
2015-09-27 19:41:04 +0200

    Chapter 16: Sum times test
    Test errors

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -73,4 +73,14 @@ describe 'Multi-Currency Money' do
     result = bank.reduce(sum, 'USD')
     expect(result).to eq(Money.dollar(15))
   end
+
+  it 'supports Sum times' do
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate('CHF', 'USD', 2)
+    sum = Sum.new(five_bucks, ten_francs).times(2)
+    result = bank.reduce(sum, 'USD')
+    expect(result).to eq(Money.dollar(20))
+  end
 end
