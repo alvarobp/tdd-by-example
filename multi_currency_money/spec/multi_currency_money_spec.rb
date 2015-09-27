489a064f4545053957951c856a79989bf3e93de8
Alvaro Bautista <alvarobp@gmail.com>
2015-09-27 19:17:59 +0200

    Chapter 15: Mixed addition test
    Test is RED
    
    This test does not error because our tests (in Ruby) are not tied to types.

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -54,4 +54,13 @@ describe 'Multi-Currency Money' do
     result = bank.reduce(Money.franc(2), 'USD')
     expect(result).to eq(Money.dollar(1))
   end
+
+  it 'supports mixed addition' do
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate('CHF', 'USD', 2)
+    result = bank.reduce(five_bucks.plus(ten_francs), 'USD')
+    expect(result).to eq(Money.dollar(10))
+  end
 end
