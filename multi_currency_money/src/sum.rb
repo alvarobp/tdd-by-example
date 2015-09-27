b99527cc040df0a56b5e2ed19cb97b58abcfeb01
Alvaro Bautista <alvarobp@gmail.com>
2015-09-27 19:20:37 +0200

    Chapter 15: Reduce augend and addend in Sum reduce
    Test is GREEN

--- multi_currency_money/src/sum.rb
+++ multi_currency_money/src/sum.rb
@@ -11,7 +11,8 @@ class Sum
   end
 
   def reduce(bank, to)
-    amount = augend.amount + addend.amount
+    amount = augend.reduce(bank, to).amount +
+      addend.reduce(bank, to).amount
     Money.new(amount, to)
   end
 end
