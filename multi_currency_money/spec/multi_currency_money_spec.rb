bb287f75a4f543becd6c6c45688cc3081aaf6679
Alvaro Bautista <alvarobp@gmail.com>
2015-09-27 19:32:35 +0200

    Chapter 16: Sum plus Money test
    Test errors

--- multi_currency_money/spec/multi_currency_money_spec.rb
+++ multi_currency_money/spec/multi_currency_money_spec.rb
@@ -63,4 +63,14 @@ describe 'Multi-Currency Money' do
     result = bank.reduce(five_bucks.plus(ten_francs), 'USD')
     expect(result).to eq(Money.dollar(10))
   end
+
+  it 'supports Sum plus Money' do
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate('CHF', 'USD', 2)
+    sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
+    result = bank.reduce(sum, 'USD')
+    expect(result).to eq(Money.dollar(15))
+  end
 end
