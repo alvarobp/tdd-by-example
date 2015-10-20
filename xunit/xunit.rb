66f04619cd40d4586164d3b6cdfd312a62e7a843
Alvaro Bautista <alvarobp@gmail.com>
2015-10-20 21:20:05 +0200

    Chapter 22: Implement test_failed
    Test is GREEN

--- xunit/xunit.rb
+++ xunit/xunit.rb
@@ -5,14 +5,19 @@ end
 class TestResult
   def initialize
     @run_count = 0
+    @error_count = 0
   end
 
   def test_started
     @run_count += 1
   end
 
+  def test_failed
+    @error_count += 1
+  end
+
   def summary
-    "#{@run_count} run, 0 failed"
+    "#{@run_count} run, #{@error_count} failed"
   end
 end
 
